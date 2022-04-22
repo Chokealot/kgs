99a88a779344da5aa90920f54dd6408762e1cfca
casper konyi <c.konyi@hotmail.com>
2022-04-22 10:50:55 +0200

    Init

---
+++ src/main/java/com/chokealot/konyiguitar/KonyiguitarApplication.java
@@ -0,0 +1,13 @@
+package com.chokealot.konyiguitar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonyiguitarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KonyiguitarApplication.class, args);
+    }
+
+}

5167af66ce0ae0fc44c3a825a796aa9f419a4251
Chokealot <casper.konyi@iths.se>
2022-04-26 16:50:49 +0200

    added real address for DB

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM --platform=linux/amd64 gradle:7.4.2-jdk17-alpine AS build
-EXPOSE 9988
+EXPOSE 8877
 COPY --chown=gradle:gradle . /home/gradle/src
 WORKDIR /home/gradle/src
 RUN gradle bootJar
 
 FROM openjdk:17-jdk-alpine
-EXPOSE 9988
+EXPOSE 8877
 RUN mkdir /jar
 COPY --from=build /home/gradle/src/build/libs/*.jar /jar/app.jar
 ENTRYPOINT ["java","-jar","/jar/app.jar"]
---
+++ src/main/java/com/chokealot/konyiguitar/health.java
@@ -0,0 +1,17 @@
+package com.chokealot.konyiguitar;
+
+import org.apache.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/health")
+public class health {
+
+    @PostMapping("")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok().build();
+    }
+}
